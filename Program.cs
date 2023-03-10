413f76aad76cf7df52f9d4499369282ed03889e6
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-11 00:05:36 +0530

    Basic Print statement

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //flipCoin.flipTheCoin();
+            DataType.display(S);
+            //Leapyear.year();
+            //Factorization.call();
+            //Harmonic.call();
+           // PowerOfN.call();
+
+           
+        }
+    }
+}
