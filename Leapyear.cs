2fcf40de757c5b4a614c708c7df94591b12c3ac2
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-11 00:16:45 +0530

    Write a code for Leap Year

---
+++ Leapyear.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Leapyear
+    {
+        public static void year()
+        {
+            Console.WriteLine("Enter the year:");
+            string input = Console.ReadLine();
+
+            int length = input.Length;
+
+
+            if(length == 4)
+            {
+                int newyear = Convert.ToInt32(input);
+
+                if (newyear % 4 == 0)
+                {
+                    Console.WriteLine("this is a leap year");
+                }
+
+                else
+                {
+                    Console.WriteLine("This is not leap year");
+                }
+            }
+              
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+
+        }
+    }
+}
