d453857cb02af6770beb548b582ff07275fb03f4
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-11 00:22:08 +0530

    Calculate Power of N

---
+++ PowerOfN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class PowerOfN
+    {
+        public static void call()
+        {
+            Console.WriteLine("Please Enter the number");
+
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i <= N; i++)
+            {
+             Console.WriteLine($"{ 2}*{ i} = { (long)Math.Pow(2,i): No}");
+            }
+        }
+    }
+}
