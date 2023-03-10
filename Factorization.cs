c57a8f6fe0f6d83ae1db3dbfcf8c112ce62a611b
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-11 00:31:28 +0530

    Write a Program for factorization

---
+++ Factorization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Factorization
+    {
+        public static void call()
+        {
+            Console.WriteLine("Enter the value for prime Factorization:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i * i < n; i++)
+            {
+                while (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    n /= i;
+                }
+            }
+            if (n>2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
